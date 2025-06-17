fc938130c313a148bf02ef0890f33b6d180d04ae
박희찬 <choni0408@gmail.com>
2025-06-17 22:27:57 +0000

    수정해

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -40,13 +40,13 @@ export interface ButtonProps
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild: isSlot = false, ...restProps }, ref) => {
-    const Comp = isSlot ? Slot : "button"
+  ({ className, variant, size, asChild, ...props }, ref) => {
+    const Comp = asChild ? Slot : "button"
     return (
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
-        {...restProps}
+        {...props}
       />
     )
   }
