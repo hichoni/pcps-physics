cb9f9f70755265c76cb7fec7e4910b3f2e22870f
박희찬 <choni0408@gmail.com>
2025-06-17 05:25:37 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/student/layout.tsx
+++ src/app/student/layout.tsx
@@ -20,7 +20,7 @@ export default function StudentLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&family=Noto+Sans+KR:wght@400;700&display=swap" rel="stylesheet" />
       </head>
-      <body className="font-body antialiased min-h-screen flex flex-col bg-background">
+      <body className="font-body antialiased min-h-screen flex flex-col">
         {children}
         <Toaster />
       </body>
